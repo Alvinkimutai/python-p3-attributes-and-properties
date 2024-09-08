98861e6ff9f81c43028354d5760bf58cc506640d
Alvinkimutai <alvinchepkiyeng@gmail.com>
2024-09-08 16:01:40 +0300

    practices on property in OOP

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,35 @@ APPROVED_JOBS = [
 ]
 
 class Person:
+    def __init__(self,name = "Susan", job = "ITC") -> None:
+        self.name = name
+        self.job = job
+        # pass
+
+    def get_name(self):
+        print("getting name")
+        return self._name
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25 :
+           self._name =  name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    name = property(get_name, set_name)
+
+    def get_job(self):
+        print("getting job")
+        return self._job
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+    job = property(get_job, set_job)
+
     pass
+
+
+alfred = Person("rex", "Marketing")
+
+print(alfred.name)
+print(alfred.job)
